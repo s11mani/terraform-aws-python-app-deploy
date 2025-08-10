fe8206f340a87ae60cf0c47cb751f99c60080f8d
Manideep <88645307+s11mani@users.noreply.github.com>
2025-08-11 02:27:58 +0530

    Create ec2_instance.tf

---
+++ ec2_instance.tf
@@ -0,0 +1,29 @@
+resource "aws_instance" "name" {
+  ami           = "ami-0f918f7e67a3323f0"
+  key_name      = aws_key_pair.name.key_name
+  instance_type = "t2.micro"
+  tags = {
+    Name = "${var.project}-instance"
+  }
+  subnet_id                   = aws_subnet.name.id
+  vpc_security_group_ids      = [aws_security_group.name.id]
+  associate_public_ip_address = true
+  connection {
+    user        = "ubuntu"
+    type        = "ssh"
+    private_key = file("./keys/id_rsa")
+    host        = self.public_ip
+  }
+  provisioner "file" {
+    source      = "./python-app/app.py"
+    destination = "/home/ubuntu/app.py"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update -y",
+      "sudo apt-get install -y python3-pip",
+      "sudo apt install python3-flask -y",
+      "sudo bash -c 'nohup python3 /home/ubuntu/app.py > /home/ubuntu/app.log 2>&1 &'"
+    ]
+  }
+}
