3ffe2291594e29c3e415392248b62b32630dedeb
Manideep <88645307+s11mani@users.noreply.github.com>
2025-08-11 02:04:47 +0530

    Update sg_keypair_ec2.tf add instance resource

--- sg_keypair_ec2.tf
+++ sg_keypair_ec2.tf
@@ -36,3 +36,33 @@ resource "aws_key_pair" "name" {
     Name = "${var.project}-keypair"
   }
 }
+
+resource "aws_instance" "name" {
+  ami           = "ami-0f918f7e67a3323f0"
+  key_name      = aws_key_pair.name.key_name
+  instance_type = "t2.micro"
+  tags = {
+    Name = "${var.project}-instance"
+  }
+  subnet_id                   = aws_subnet.name.id
+  vpc_security_group_ids      = [aws_security_group.name.id]
+  associate_public_ip_address = true
+  connection {
+    user        = "ubuntu"
+    type        = "ssh"
+    private_key = file("/terraform-prac/test/id_rsa")
+    host        = self.public_ip
+  }
+  provisioner "file" {
+    source      = "/terraform-prac/test/python-app/app.py"
+    destination = "/home/ubuntu/app.py"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update -y",
+      "sudo apt-get install -y python3-pip",
+      "sudo pip3 install flask",
+      "nohup sudo python3 /home/ubuntu/app.py > /home/ubuntu/app.log 2>&1 &"
+    ]
+  }
+}
