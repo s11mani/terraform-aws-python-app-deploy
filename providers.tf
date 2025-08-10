8ee41852b5938d36be028479edc861e1cb608ab0
Manideep <88645307+s11mani@users.noreply.github.com>
2025-08-11 00:39:16 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,1 +1,13 @@
-###
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.8.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+  alias  = "mumbai"
+}
