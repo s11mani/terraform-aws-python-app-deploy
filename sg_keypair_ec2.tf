9a31d1b8e9e47a4748bb723970bdf6e43f13d46b
Manideep <88645307+s11mani@users.noreply.github.com>
2025-08-11 01:39:00 +0530

    Update sg_keypair_ec2.tf

--- sg_keypair_ec2.tf
+++ sg_keypair_ec2.tf
@@ -30,7 +30,7 @@ resource "aws_security_group" "name" {
 
 
 resource "aws_key_pair" "name" {
-  public_key = file("/terraform-prac/test/id_rsa.pub")
+  public_key = file("./keys/id_rsa.pub")
   key_name   = "${var.project}-keypair"
   tags = {
     Name = "${var.project}-keypair"
